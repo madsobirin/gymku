6a250d2e96b72bae4675975ddeaf1825f1af4092
madsobirin <gamingbrn202@gmail.com>
2026-05-24 15:48:30 +0700

    deploy fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,16 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString =
+  process.env.DATABASE_URL ||
+  process.env.POSTGRES_PRISMA_URL ||
+  process.env.POSTGRES_URL;
+
+if (!connectionString) {
+  throw new Error(
+    "Missing database connection string. Please set DATABASE_URL or POSTGRES_PRISMA_URL in your environment variables.",
+  );
+}
 
 const adapter = new PrismaPg({
   connectionString,
